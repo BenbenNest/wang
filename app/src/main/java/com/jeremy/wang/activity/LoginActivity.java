4179381948dbf8ed97fcfbacf65023d516aba09c
jeremy <zhaochangqing@didichuxing.com>
2018-01-22 22:05:12 +0800

    add basic layout of login and register

--- app/src/main/java/com/jeremy/wang/activity/LoginActivity.java
+++ app/src/main/java/com/jeremy/wang/activity/LoginActivity.java
@@ -1,20 +1,36 @@
 package com.jeremy.wang.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.jeremy.wang.R;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView bt_register;
+    TextView bt_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initView();
 
     }
 
-
+    private void initView() {
+        bt_register = findViewById(R.id.bt_register);
+        bt_login = findViewById(R.id.bt_login);
+        bt_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
 
 }
--- app/src/main/java/com/jeremy/wang/view/CommonInputLayout.java
+++ app/src/main/java/com/jeremy/wang/view/CommonInputLayout.java
@@ -1,6 +1,7 @@
 package com.jeremy.wang.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -18,18 +19,26 @@ public class CommonInputLayout extends LinearLayout {
 
     public CommonInputLayout(Context context) {
         super(context);
-        init(context);
+        init(context, null);
     }
 
     public CommonInputLayout(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+
+        init(context, attrs);
     }
 
 
-    private void init(Context context) {
+    private void init(Context context, AttributeSet attrs) {
         LayoutInflater.from(context).inflate(R.layout.common_input_layout, this, true);
-
+        mInputText = findViewById(R.id.input_edit);
+        if (attrs != null) {
+            TypedArray typedArray = getResources().obtainAttributes(attrs, R.styleable.CommonInputLayout);
+            String hint = typedArray.getString(R.styleable.CommonInputLayout_input_hint);
+            if (mInputText != null) {
+                mInputText.setText(hint);
+            }
+        }
     }
 
 
