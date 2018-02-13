8fb1d55c1291b0114fce50996c8347b767d8c529
jeremy <zhaochangqing@didichuxing.com>
2018-02-13 09:24:44 +0800

    add idcardscan

--- app/src/main/java/com/nine/finance/activity/IDCardActivity.java
+++ app/src/main/java/com/nine/finance/activity/IDCardActivity.java
@@ -1,6 +1,7 @@
 package com.nine.finance.activity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 import com.nine.finance.R;
 import com.nine.finance.api.OnBooleanListener;
 import com.nine.finance.idcard.AuthManager;
+import com.nine.finance.idcard.IDCardScanActivity;
+import com.nine.finance.idcard.util.Screen;
 
 public class IDCardActivity extends BaseActivity implements AuthManager.AuthCallBack {
 
@@ -19,6 +22,7 @@ public class IDCardActivity extends BaseActivity implements AuthManager.AuthCall
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_idcard);
+        Screen.initialize(this);
         init();
     }
 
@@ -77,10 +81,21 @@ public class IDCardActivity extends BaseActivity implements AuthManager.AuthCall
         }
     };
 
+    private static final int REQUEST_IDCARDSCAN_CODE = 100;
+
     @Override
     public void authState(boolean flag) {
         if (true) {
-            startActivity(IDCardActivity.this, IDCardUploadActivity.class);
+//            startActivity(IDCardActivity.this, IDCardScanActivity.class);
+            Intent intent = new Intent(this, IDCardScanActivity.class);
+            intent.putExtra("isvertical", true);
+            intent.putExtra("isClearShadow", false);
+            intent.putExtra("isTextDetect", false);
+            intent.putExtra("isDebug", false);
+            intent.putExtra("bound", 0.8);
+            intent.putExtra("idcard", 0.1);
+            intent.putExtra("clear", 0.8);
+            startActivityForResult(intent, REQUEST_IDCARDSCAN_CODE);
         }
     }
 }
